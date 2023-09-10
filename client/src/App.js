c35280f1b690a03b573b7960706bcbf415ec66ea
Can Kayalı <volkand135@gmail.com>
2023-09-10 16:50:45 +0300

    QRMENUV2

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="App">
-     
+     <Sidebar />
     </div>
   );
 }
